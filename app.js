018183f88f7e2000cd156d2404e1b147443bc54a
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-11-04 23:13:21 +0530

    Created budgetController and update UI

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Module pattern -- IIFE and Closures 
+// Module pattern -- IIFE and Closures
 
 /* ******************************** SAMPLE TEST **************************
 var budgetController = (
@@ -38,77 +38,140 @@ var appController = (function(budgerCtrl, UICtrl){
 
 *****************************************************************************************************/
 
-var budgetController = (
-    function(){
-        // write code here
-    }
-)();
+var budgetController = (function () {
+  // Need to have data model for income and expense here.
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-var UIController = (
-    
-    function(){
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 
-        // Too many query selectors, to prevent changes make an object with them.
-        var DOMstrings = {
-            inputType: '.add__type',
-            inputDescription: '.add__description',
-            inputValue: '.add__value',
-            inputBtn: '.add__btn'
-        }
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      /*    ID needs to be unique and not cause clash on deletion or add.
+            [1 2 3 4 5] , next Id = 6
+            [1 2 4 6 8] , next Id = 9
+            ID = last Id + 1
+      */
 
-    return {
-        getInput: function(){
-            // use return fn an object as we need to return more variables.
-            return {
-                // type values are - inc(+) or exp(-)
-                type : document.querySelector(DOMstrings.inputType).value,
-                description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
-            };
-        },
+      // Create new ID
+      data.allItems[type].length > 0
+        ? (ID = data.allItems[type][data.allItems[type].length - 1].id + 1)
+        : (ID = 0);
 
-        getDOMstrings: function(){
-            return{
-                DOMstrings: DOMstrings
-            };
-        }
-    }
+      // Create new Item based on 'inc' or 'exp' type
+      type === "exp"
+        ? (newItem = new Expense(ID, des, val))
+        : type === "inc"
+        ? (newItem = new Income(ID, des, val))
+        : "";
 
-    }
-)();
+      // Push it into our Data Structure
+      data.allItems[type].push(newItem);
 
+      // Return the new Element
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    },
+  };
+})();
 
-var appController = (
-    
-    function(budgerCtrl, UICtrl){
+var UIController = (function () {
+  // Too many query selectors, to prevent changes make an object with them.
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
 
-    var DOM = UICtrl.getDOMstrings();    
-    var ctrlAddItem = function(){
+  return {
+    getInput: function () {
+      // use return fn an object as we need to return more variables.
+      return {
+        // type values are - inc(+) or exp(-)
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
 
-        // 1. get field input data
-        var input = UICtrl.getInput();
-        // 2. Add the item to budget contrl
-        // 3. add items to UI
-        // 4. Calc budget 
-        // 5. Disp the budget
-        console.log(DOM);
-    } 
+    // add new items to UI
+    addListItem: function (obj, type) {
+      var html;
 
-    document.querySelector(DOM.DOMstrings.inputBtn).addEventListener('click',ctrlAddItem)
+      type === "inc"
+        ? // Create HTML string with placeholder text
+          (html = `<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`)
+        : type === "exp"
+        ? (html = `<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`)
+        : "";
 
+      // Replace placeholder text with data recieve from object
+
+      // insert html to DOM
+    },
+
+    getDOMstrings: function () {
+      return {
+        DOMstrings: DOMstrings,
+      };
+    },
+  };
+})();
+
+var appController = (function (budgerCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+    document
+      .querySelector(DOM.DOMstrings.inputBtn)
+      .addEventListener("click", ctrlAddItem);
     // For enter keypress event listener
-    document.addEventListener('keypress', function(event){
-
-        if(event.key === 'Enter'){
-            
-             ctrlAddItem();
-        }
+    document.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        ctrlAddItem();
+      }
     });
-    }
+  };
 
-)(budgetController, UIController);
+  var ctrlAddItem = function () {
+    var input, newItem;
+    // 1. get field input data
+    input = UICtrl.getInput();
 
+    // 2. Add the item to budget contrl
+    newItem = budgerCtrl.addItem(input.type, input.description, input.value);
 
+    // 3. add items to UI
+    // 4. Calc budget
+    // 5. Disp the budget
+  };
 
+  return {
+    init: function () {
+      console.log("Application has started!");
+      setupEventListeners();
+    },
+  };
+})(budgetController, UIController);
 
-
+appController.init();
